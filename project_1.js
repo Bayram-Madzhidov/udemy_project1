7e1aa00c66add42b8f68b9d7ff012a4b4f8b40b4
Bayram <example@gmail.com>
2023-04-13 19:03:26 +0300

    add 2 lesson

--- project_1.js
+++ project_1.js
@@ -10,12 +10,27 @@ const personalMovieDB = {
   privat: false,
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", "");
-const b = prompt("На сколько оцените его?", "");
-const c = prompt("Один из последних просмотренных фильмов?", "");
-const d = prompt("На сколько оцените его?", "");
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последних просмотренных фильмов?", "");
+  const b = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("done");
+  } else {
+    i--;
+    console.log("error");
+  }
+}
 
 console.log(personalMovieDB);
+
+if (numberOfFilms < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+} else if (numberOfFilms >= 10 && numberOfFilms < 30) {
+  console.log("Вы классный зритель");
+} else if (numberOfFilms > 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Произошла ошибка");
+}
